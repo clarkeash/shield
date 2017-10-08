4118a668d662c3ed20cd5962d66cfaf0eb10703d
Thomas Farla <thomas-farla@hotmail.com>
2017-10-08 21:39:15 +0200

    Add trello

--- resources/config/shield.php
+++ resources/config/shield.php
@@ -6,6 +6,7 @@ return [
         'gitlab' => \Clarkeash\Shield\Services\GitLab::class,
         'stripe' => \Clarkeash\Shield\Services\Stripe::class,
         'zapier' => \Clarkeash\Shield\Services\Zapier::class,
+        'trello' => \Clarkeash\Shield\Services\Trello::class
     ],
 
     'services' => [
@@ -22,6 +23,9 @@ return [
         'zapier' => [
             'username' => 'your-basic-auth-user',
             'password' => 'your-basic-auth-password',
+        ],
+        'trello' => [
+            'app_secret' => 'your-app-secret'
         ]
     ]
 ];
